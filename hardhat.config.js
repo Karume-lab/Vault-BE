5bd6e810d955fc02ff0c3ee83419fe5e21378cdc
Karume-lab <danielkarume@outlook.com>
2024-03-21 16:41:46 +0300

    first try

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+// require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
+
+// const SEPOLIA_URL = process.env.SEPOLIA_URL;
+// const PRIVATE_KEY = process.env.PRIVATE_KEY;
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    // sepolia: {
+    // url: SEPOLIA_URL,
+    // accounts: [PRIVATE_KEY],
+  },
+  paths: {
+    artifacts: "./client/src//artifacts",
+  },
+  // },
+  // etherscan: {    // to verify contract in CLI way（也有GUI的，可以在etherscan ui 那边进行verify，只不过CLI 比较方便 一行command 解决）
+  //   apiKey: process.env.ETHERSCAN_API_KEY,
+  // }
+};
