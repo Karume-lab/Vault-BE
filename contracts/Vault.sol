c8095ff45313d96663f62c6b9dae09bb0d5b9444
Karume-lab <danielkarume@outlook.com>
2024-03-31 14:06:30 +0300

    sharing

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -45,6 +45,19 @@ contract Vault {
     // Stores the previous access value
     mapping(address => mapping(address => bool)) PreviousAccess;
 
+    // Events to log file sharing and access changes
+    event FileShared(
+        address indexed fileOwner,
+        address indexed sharedWith,
+        string cid,
+        bool hasAccess
+    );
+    event AccessChanged(
+        address indexed fileOwner,
+        address indexed sharedWith,
+        bool hasAccess
+    );
+
     function uploadFile(
         address _owner,
         string memory _name,
@@ -98,31 +111,6 @@ contract Vault {
         revert("File with the given CID not found");
     }
 
-    // share a file in your vault to an address
-    function shareFile(address user) external {
-        MyVault[msg.sender][user] = true;
-        if (PreviousAccess[msg.sender][user]) {
-            for (uint i = 0; i < AccessList[msg.sender].length; i++) {
-                if (AccessList[msg.sender][i].user == user) {
-                    AccessList[msg.sender][i].hasAccess = true;
-                }
-            }
-        } else {
-            AccessList[msg.sender].push(Access(user, true));
-            PreviousAccess[msg.sender][user] = true;
-        }
-    }
-
-    // unshare a file in your vault from an address
-    function unshareFile(address user) public {
-        MyVault[msg.sender][user] = false;
-        for (uint i = 0; i < AccessList[msg.sender].length; i++) {
-            if (AccessList[msg.sender][i].user == user) {
-                AccessList[msg.sender][i].hasAccess = false;
-            }
-        }
-    }
-
     // get the tags in the enum
     function getTags() public pure returns (string[] memory) {
         string[] memory tags = new string[](7);
@@ -139,8 +127,80 @@ contract Vault {
         return tags;
     }
 
-    // retrieve all the users with access to your vault
-    function getUsersWithAccess() public view returns (Access[] memory) {
-        return AccessList[msg.sender];
+    // Function to toggle a user's access to another user's file
+    function toggleAccess(
+        address _fileOwner,
+        string memory _cid,
+        address _sharedWith
+    ) external {
+        bool fileFound = false;
+        uint256 fileIndex;
+        // Loop through the files of the file owner to find the specified file
+        for (uint256 i = 0; i < Files[_fileOwner].length; i++) {
+            if (
+                keccak256(abi.encodePacked(Files[_fileOwner][i].cid)) ==
+                keccak256(abi.encodePacked(_cid))
+            ) {
+                fileFound = true;
+                fileIndex = i;
+                break;
+            }
+        }
+        require(fileFound, "File with the given CID not found");
+
+        // Check if the caller is the file owner
+        require(
+            msg.sender == _fileOwner,
+            "Only the file owner can toggle access"
+        );
+
+        // Toggle access for the shared user
+        if (!MyVault[_fileOwner][_sharedWith]) {
+            AccessList[_fileOwner].push(Access(_sharedWith, true));
+        } else {
+            for (uint256 j = 0; j < AccessList[_fileOwner].length; j++) {
+                if (AccessList[_fileOwner][j].user == _sharedWith) {
+                    AccessList[_fileOwner][j].hasAccess = !AccessList[
+                        _fileOwner
+                    ][j].hasAccess;
+                    break;
+                }
+            }
+        }
+        // Update the mapping of access
+        MyVault[_fileOwner][_sharedWith] = !MyVault[_fileOwner][_sharedWith];
+        emit AccessChanged(
+            _fileOwner,
+            _sharedWith,
+            MyVault[_fileOwner][_sharedWith]
+        );
+    }
+
+    // Function to return all the files shared to a user
+    function getSharedFiles(
+        address _user
+    ) external view returns (File[] memory) {
+        require(_user != address(0), "Invalid address");
+
+        uint256 sharedCount = 0;
+        for (uint256 i = 0; i < AccessList[_user].length; i++) {
+            if (AccessList[_user][i].hasAccess) {
+                sharedCount += Files[AccessList[_user][i].user].length;
+            }
+        }
+
+        File[] memory sharedFiles = new File[](sharedCount);
+        uint256 index = 0;
+        for (uint256 i = 0; i < AccessList[_user].length; i++) {
+            if (AccessList[_user][i].hasAccess) {
+                address sharedWith = AccessList[_user][i].user;
+                for (uint256 j = 0; j < Files[sharedWith].length; j++) {
+                    sharedFiles[index] = Files[sharedWith][j];
+                    index++;
+                }
+            }
+        }
+
+        return sharedFiles;
     }
 }
