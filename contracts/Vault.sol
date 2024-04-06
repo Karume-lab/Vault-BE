d9c149244e0a7ee526a09beae00fc04413eaeb37
Karume-lab <danielkarume@outlook.com>
2024-04-06 16:29:49 +0300

    add share, delete and archive features

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,9 +1,12 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-pragma solidity >=0.7.0 <0.9.0;
-
+/**
+ * @title Vault
+ * @dev A smart contract for storing files on the blockchain and managing file-related operations.
+ */
 contract Vault {
-    // tags to be used for files
+    // Enum for file tags
     enum Tags {
         OTHER,
         EDUCATION,
@@ -14,104 +17,286 @@ contract Vault {
         RANDOM
     }
 
-    // contains metadata for each file
+    /**
+     * @title File
+     * @dev Struct representing a file, containing various attributes such as owner, name, description, etc.
+     */
     struct File {
-        address owner;
-        uint dateUploaded;
-        uint dateModified;
-        uint dateAccessed;
-        bool isFavourite;
-        bool isArchived;
-        string cid;
-        string name;
-        string description;
-        string extension;
-        string size;
-        Tags tag;
+        address owner; // Address of the file owner
+        string name; // Name of the file
+        string description; // Description of the file
+        uint256 size; // Size of the file
+        uint256 dateUploaded; // Timestamp when the file was uploaded
+        uint256 dateAccessed; // Timestamp when the file was last accessed
+        uint256 dateDeleted; // Timestamp when the file was marked as deleted
+        uint256 dateModified; // Timestamp when the file was last modified
+        string cid; // IPFS CID of the file
+        bool isFavourite; // Flag indicating if the file is marked as favorite
+        bool isArchived; // Flag indicating if the file is archived
+        bool isDeleted; // Flag indicating if the file is deleted
+        string extension; // File extension
+        Tags tag; // Tag of the file
     }
 
-    // defines who has access to a user's vault
-    struct Access {
-        address user;
-        bool hasAccess;
+    /**
+     * @title SharedFile
+     * @dev Struct to represent a shared file, containing details such as IPFS CID, owner, and recipient
+     */
+    struct SharedFile {
+        string cid; // IPFS CID of the shared file
+        address owner; // Address of the owner who shared the file
+        address recipient; // Address of the recipient who received the file
     }
 
-    // stores a user's file metadata
-    mapping(address => File[]) Files;
-    // stores the logged in user's files
-    mapping(address => mapping(address => bool)) MyVault;
-    // Defines the people who have access to your vault
-    mapping(address => Access[]) AccessList;
-    // Stores the previous access value
-    mapping(address => mapping(address => bool)) PreviousAccess;
+    // Mapping to store user files
+    mapping(address => mapping(string => File)) userFiles;
 
-    // Events to log file sharing and access changes
-    event FileShared(
-        address indexed fileOwner,
-        address indexed sharedWith,
-        string cid,
-        bool hasAccess
-    );
-    event AccessChanged(
-        address indexed fileOwner,
-        address indexed sharedWith,
-        bool hasAccess
-    );
+    // Mapping to store shared files using the SharedFile struct
+    mapping(string => SharedFile[]) sharedFiles;
 
+    // Mapping to track shared files with specific users
+    mapping(string => mapping(address => bool)) sharedWith;
+
+    // Mapping to track deleted files
+    mapping(string => bool) deletedFiles;
+
+    // Array to store all file keys
+    string[] private allKeys;
+
+    // Modifier to check if the file exists
+    modifier fileExists(address _owner, string memory _cid) {
+        require(userFiles[_owner][_cid].owner == _owner, "File does not exist");
+        _;
+    }
+
+    // Modifier to check if the file is not deleted
+    modifier fileNotDeleted(string memory _cid) {
+        require(!deletedFiles[_cid], "File is already deleted");
+        _;
+    }
+
+    /**
+     * @dev Uploads a file to the blockchain.
+     * @param _cid IPFS CID of the file
+     * @param _name Name of the file
+     * @param _description Description of the file
+     * @param _isFavourite Flag indicating if the file is marked as favorite
+     * @param _size Size of the file
+     * @param _extension File extension
+     * @param _tag Tag of the file
+     */
     function uploadFile(
-        address _owner,
+        string memory _cid,
         string memory _name,
         string memory _description,
-        string memory _size,
-        string memory _extension,
         bool _isFavourite,
-        Tags _tag,
-        string memory _cid
+        uint256 _size,
+        string memory _extension,
+        Tags _tag
     ) external {
-        File memory file = File({
-            owner: _owner,
-            dateUploaded: block.timestamp,
-            dateModified: 0,
-            dateAccessed: 0,
-            isFavourite: _isFavourite,
-            isArchived: false,
-            size: _size,
-            cid: _cid,
+        require(
+            bytes(userFiles[msg.sender][_cid].cid).length == 0,
+            "File already exists"
+        );
+        userFiles[msg.sender][_cid] = File({
+            owner: msg.sender,
             name: _name,
             description: _description,
+            size: _size,
+            dateUploaded: block.timestamp,
+            dateAccessed: block.timestamp,
+            dateModified: block.timestamp,
+            dateDeleted: 0,
+            cid: _cid,
+            isFavourite: _isFavourite,
+            isArchived: false,
+            isDeleted: false,
             extension: _extension,
             tag: _tag
         });
-        Files[msg.sender].push(file);
+        allKeys.push(_cid);
     }
 
-    // get all files for the connected wallet
-    function getFiles(address _user) external view returns (File[] memory) {
-        require(
-            _user == msg.sender || MyVault[_user][msg.sender],
-            "You don't have access"
-        );
-        return Files[_user];
-    }
+    /**
+     * @dev Gets all files for the caller's address.
+     * @return Array of file objects.
+     */
+    function getFiles() external returns (File[] memory) {
+        File[] memory files = new File[](allKeys.length);
 
-    // mark or unmark a file as a favourite and return the CID
-    function toggleFavourite(
-        string memory _cid
-    ) external returns (string memory) {
-        for (uint i = 0; i < Files[msg.sender].length; i++) {
-            if (
-                keccak256(abi.encodePacked(Files[msg.sender][i].cid)) ==
-                keccak256(abi.encodePacked(_cid))
-            ) {
-                Files[msg.sender][i].isFavourite = !Files[msg.sender][i]
-                    .isFavourite;
-                return _cid;
+        uint256 index = 0;
+        for (uint256 i = 0; i < allKeys.length; i++) {
+            string memory cid = allKeys[i];
+            if (userFiles[msg.sender][cid].owner == msg.sender) {
+                userFiles[msg.sender][cid].dateAccessed = block.timestamp;
+                files[index] = userFiles[msg.sender][cid];
+                index++;
             }
         }
-        revert("File with the given CID not found");
+
+        return files;
     }
 
-    // get the tags in the enum
+    /**
+     * @dev Gets a specific file for the caller's address.
+     * @param _cid IPFS CID of the file.
+     * @return File object.
+     */
+    function getFile(string memory _cid) external returns (File memory) {
+        userFiles[msg.sender][_cid].dateAccessed = block.timestamp;
+        return userFiles[msg.sender][_cid];
+    }
+
+    /**
+     * @dev Edits the details of a file.
+     * @param _cid IPFS CID of the file.
+     * @param _name New name of the file.
+     * @param _description New description of the file.
+     * @param _tag New tag of the file.
+     * @param _extension New extension of the file.
+     */
+    function editFile(
+        string memory _cid,
+        string memory _name,
+        string memory _description,
+        Tags _tag,
+        string memory _extension
+    ) external fileExists(msg.sender, _cid) {
+        File storage file = userFiles[msg.sender][_cid];
+        file.name = _name;
+        file.description = _description;
+        file.tag = _tag;
+        file.extension = _extension;
+        file.dateModified = block.timestamp;
+    }
+
+    /**
+     * @dev Updates the access timestamp of a file.
+     * @param _cid IPFS CID of the file.
+     */
+    function updateDateAccessed(
+        string memory _cid
+    ) external fileExists(msg.sender, _cid) {
+        userFiles[msg.sender][_cid].dateAccessed = block.timestamp;
+    }
+
+    /**
+     * @dev Updates the modification timestamp of a file.
+     * @param _cid IPFS CID of the file.
+     */
+    function updateDateModified(
+        string memory _cid
+    ) external fileExists(msg.sender, _cid) {
+        userFiles[msg.sender][_cid].dateModified = block.timestamp;
+    }
+
+    /**
+     * @dev Toggles the favorite status of a file.
+     * @param _cid IPFS CID of the file.
+     */
+    function toggleFavourite(
+        string memory _cid
+    ) external fileExists(msg.sender, _cid) {
+        userFiles[msg.sender][_cid].isFavourite = !userFiles[msg.sender][_cid]
+            .isFavourite;
+        userFiles[msg.sender][_cid].dateModified = block.timestamp;
+    }
+
+    /**
+     * @dev Toggles the archive status of a file.
+     * @param _cid IPFS CID of the file.
+     */
+    function toggleArchive(
+        string memory _cid
+    ) external fileExists(msg.sender, _cid) {
+        userFiles[msg.sender][_cid].isArchived = !userFiles[msg.sender][_cid]
+            .isArchived;
+        userFiles[msg.sender][_cid].dateModified = block.timestamp;
+    }
+
+    /**
+     * @dev Toggles the deletion status of a file.
+     * @param _cid IPFS CID of the file.
+     */
+    function toggleDelete(
+        string memory _cid
+    ) external fileExists(msg.sender, _cid) {
+        if (userFiles[msg.sender][_cid].isDeleted) {
+            // If the file is already marked as deleted, unmark it
+            userFiles[msg.sender][_cid].isDeleted = false;
+            deletedFiles[_cid] = false;
+            userFiles[msg.sender][_cid].dateModified = block.timestamp;
+            userFiles[msg.sender][_cid].dateDeleted = 0;
+        } else {
+            // If the file is not marked as deleted, mark it as deleted
+            userFiles[msg.sender][_cid].isDeleted = true;
+            userFiles[msg.sender][_cid].dateDeleted = block.timestamp;
+            deletedFiles[_cid] = true;
+        }
+    }
+
+    /**
+     * @dev Shares a file with another user.
+     * @param _cid IPFS CID of the file to be shared.
+     * @param _user Address of the user to share the file with.
+     */
+    function shareFile(
+        string memory _cid,
+        address _user
+    ) external fileExists(msg.sender, _cid) {
+        require(_user != address(0), "Invalid user address");
+        SharedFile memory sharedFile = SharedFile({
+            cid: _cid,
+            owner: msg.sender,
+            recipient: _user
+        });
+        sharedFiles[_cid].push(sharedFile);
+        sharedWith[_cid][_user] = true;
+        userFiles[msg.sender][_cid].dateAccessed = block.timestamp;
+    }
+
+    /**
+     * @dev Unshares a file from a user.
+     * @param _cid IPFS CID of the file to be unshared.
+     * @param _user Address of the user to unshare the file from.
+     */
+    function unshareFile(
+        string memory _cid,
+        address _user
+    ) external fileExists(msg.sender, _cid) {
+        require(sharedWith[_cid][_user], "File is not shared with this user");
+        SharedFile[] storage files = sharedFiles[_cid];
+        for (uint256 i = 0; i < files.length; i++) {
+            if (files[i].recipient == _user) {
+                files[i] = files[files.length - 1];
+                files.pop();
+                sharedWith[_cid][_user] = false;
+                break;
+            }
+        }
+        userFiles[msg.sender][_cid].dateModified = block.timestamp;
+    }
+
+    /**
+     * @dev Lists all users with whom a file is shared.
+     * @param _cid IPFS CID of the file.
+     * @return Array of addresses of users with whom the file is shared.
+     */
+    function getFileAccessors(
+        string memory _cid
+    ) external view returns (address[] memory) {
+        SharedFile[] storage files = sharedFiles[_cid];
+        address[] memory accessors = new address[](files.length);
+        for (uint256 i = 0; i < files.length; i++) {
+            accessors[i] = files[i].recipient;
+        }
+        return accessors;
+    }
+
+    /**
+     * @dev Retrieves the names of all enum tags.
+     * @return Array of tag names.
+     */
     function getTags() public pure returns (string[] memory) {
         string[] memory tags = new string[](7);
 
@@ -127,80 +312,40 @@ contract Vault {
         return tags;
     }
 
-    // Function to toggle a user's access to another user's file
-    function toggleAccess(
-        address _fileOwner,
-        string memory _cid,
-        address _sharedWith
-    ) external {
-        bool fileFound = false;
-        uint256 fileIndex;
-        // Loop through the files of the file owner to find the specified file
-        for (uint256 i = 0; i < Files[_fileOwner].length; i++) {
-            if (
-                keccak256(abi.encodePacked(Files[_fileOwner][i].cid)) ==
-                keccak256(abi.encodePacked(_cid))
-            ) {
-                fileFound = true;
-                fileIndex = i;
-                break;
-            }
-        }
-        require(fileFound, "File with the given CID not found");
+    /**
+     * @dev Retrieves all files shared with the connected wallet address.
+     * @return Array of SharedFile structs representing files shared with the connected wallet address.
+     */
+    function getFilesSharedWithMe()
+        external
+        view
+        returns (SharedFile[] memory)
+    {
+        uint256 totalFilesShared = 0;
 
-        // Check if the caller is the file owner
-        require(
-            msg.sender == _fileOwner,
-            "Only the file owner can toggle access"
+        // Count the total number of files shared with the connected wallet address
+        for (uint256 i = 0; i < allKeys.length; i++) {
+            string memory cid = allKeys[i];
+            totalFilesShared += sharedFiles[cid].length;
+        }
+
+        // Create an array to store shared files
+        SharedFile[] memory sharedFilesWithMe = new SharedFile[](
+            totalFilesShared
         );
-
-        // Toggle access for the shared user
-        if (!MyVault[_fileOwner][_sharedWith]) {
-            AccessList[_fileOwner].push(Access(_sharedWith, true));
-        } else {
-            for (uint256 j = 0; j < AccessList[_fileOwner].length; j++) {
-                if (AccessList[_fileOwner][j].user == _sharedWith) {
-                    AccessList[_fileOwner][j].hasAccess = !AccessList[
-                        _fileOwner
-                    ][j].hasAccess;
-                    break;
-                }
-            }
-        }
-        // Update the mapping of access
-        MyVault[_fileOwner][_sharedWith] = !MyVault[_fileOwner][_sharedWith];
-        emit AccessChanged(
-            _fileOwner,
-            _sharedWith,
-            MyVault[_fileOwner][_sharedWith]
-        );
-    }
-
-    // Function to return all the files shared to a user
-    function getSharedFiles(
-        address _user
-    ) external view returns (File[] memory) {
-        require(_user != address(0), "Invalid address");
-
-        uint256 sharedCount = 0;
-        for (uint256 i = 0; i < AccessList[_user].length; i++) {
-            if (AccessList[_user][i].hasAccess) {
-                sharedCount += Files[AccessList[_user][i].user].length;
-            }
-        }
-
-        File[] memory sharedFiles = new File[](sharedCount);
         uint256 index = 0;
-        for (uint256 i = 0; i < AccessList[_user].length; i++) {
-            if (AccessList[_user][i].hasAccess) {
-                address sharedWith = AccessList[_user][i].user;
-                for (uint256 j = 0; j < Files[sharedWith].length; j++) {
-                    sharedFiles[index] = Files[sharedWith][j];
+
+        // Populate the array with shared files
+        for (uint256 i = 0; i < allKeys.length; i++) {
+            string memory cid = allKeys[i];
+            for (uint256 j = 0; j < sharedFiles[cid].length; j++) {
+                if (sharedFiles[cid][j].recipient == msg.sender) {
+                    sharedFilesWithMe[index] = sharedFiles[cid][j];
                     index++;
                 }
             }
         }
 
-        return sharedFiles;
+        return sharedFilesWithMe;
     }
 }
